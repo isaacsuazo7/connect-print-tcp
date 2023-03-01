d5bcac6a6b82db25aa6cc5acdf956b301517e82e
Isaac Suazo <isaac.suazo@cit.hn>
2023-03-01 10:31:55 -0600

    version 1.1.0

--- index.js
+++ index.js
@@ -1,61 +1,120 @@
-const Net = require("net");
-// const EscPosEncoder = require("esc-pos-encoder");
+const net = require("net");
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const ip = require("ip");
+const network = require("network");
+var dialog = require("dialog-node");
 
 const app = express();
+const hostPort = 3000;
+let hostname = ip.address();
+
+let dialogActive = false;
+
 app.use(cors());
 app.use(bodyParser.json());
 
+// POST PRINT
+
 app.post("/print", async (req, res) => {
   try {
-    const {host, port, toPrint} = req.body;
-    console.log(req.body);
-    const uint8Array = new Uint8Array(Buffer.from(toPrint, 'base64'));
-    await printTest(host, port, uint8Array);
-
+    const { printerHost, printerPort, dataToPrint } = req.body;
+    const uint8Array = new Uint8Array(Buffer.from(dataToPrint, "base64"));
+    await print(printerHost, printerPort, uint8Array);
     res.status(200).send({ message: "Impresión exitosa" });
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ message: "Error al imprimir" });
+    console.error(error);
+    res.status(500).send({ message: error });
   }
 });
 
-async function printTest(host, port, uint8Array) {
+// CONNECT TCP PRINTER AND PRINT
+
+const print = async (printerHost, printerPort, dataToPrint) => {
   return new Promise((resolve, reject) => {
-    const client = new Net.Socket();
+    const client = new net.Socket();
 
-    client.connect({ port: port, host: host }, () => {
-      console.log(`TCP connection established with ${host}:${port}.`);
-
-      client.write(
-        uint8Array,
-        () => {
-          console.log("Impresión exitosa");
-          client.end();
-          resolve();
-        },
-        (err) => {
-          console.log(err);
-          client.end();
-          reject(err);
-        }
-      );
-    });
-
-    client.on("error", (error) => {
-      console.log(`Error de conexión con ${host}:${port}.`);
-      client.end();
+    client.on("error", (err) => {
+      const error = `Error al conectar con la impresora http://${printerHost}/${printerPort}, verifica que el Servicio de red de Impresoras YALO este conectado a la misma red que la impresora.`;
+      showDialogError("Error", error);
       reject(error);
     });
+
+    client.connect({ port: printerPort, host: printerHost }, () => {
+      console.log(
+        `Conexión TCP establecida en http://${printerHost}/${printerPort}.`
+      );
+
+      client.write(dataToPrint, () => {
+        console.log("Impresión exitosa");
+        client.end();
+        resolve();
+      });
+    });
   });
-}
+};
 
-const PORT = 3000;
-const HOSTNAME = ip.address();
+// START SERVER
 
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`Servidor ejecutandose en: http://${HOSTNAME}/${PORT}`);
-});
+const startServer = () => {
+  server = app.listen(hostPort, hostname, () => {
+    console.log(`Servidor ejecutándose en: http://${hostname}:${hostPort}`);
+    showDialogInfo(
+      `Servicio de red de Impresoras YALO`,
+      `El servicio de red de impresoras YALO se esta ejecutando en la red http://${hostname}/${hostPort} , verifica que esta sea la red correcta.`
+    );
+  });
+};
+
+startServer();
+
+// RESTART SERVER
+
+const restartServer = () => {
+  if (server) {
+    server.close(() => {
+      console.log("Servicio de red de Impresoras YALO reiniciado.");
+      startServer();
+    });
+  } else {
+    startServer();
+  }
+};
+
+setInterval(() => {
+  network.get_active_interface((err, iface) => {
+    if (err) {
+      console.error(err);
+      showDialogError(
+        "Servicio de red de Impresoras YALO",
+        "Servicio de red de Impresoras YALO se detuvo ya que no tiene conexión a la red."
+      );
+      return;
+    }
+
+    if (hostname !== iface.ip_address) {
+      console.log(
+        `Current HOSTNAME ${hostname} === Current IP ADDRESS ${iface.ip_address}`
+      );
+      hostname = iface.ip_address;
+      restartServer();
+    }
+  });
+}, 15000);
+
+// Dialogs
+
+const showDialogInfo = (title, message) => {
+  if (!dialogActive) {
+    dialogActive = true;
+    dialog.info(message, title, 0, () => dialogActive = false);
+  }
+};
+
+const showDialogError = (title, message) => {
+  if (!dialogActive) {
+    dialogActive = true;
+    dialog.error(message, title, 0, () => dialogActive = false);
+  }
+};
